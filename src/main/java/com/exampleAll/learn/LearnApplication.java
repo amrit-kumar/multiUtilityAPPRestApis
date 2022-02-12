9d4d9b72c1637c1dbd8e9947a6e72fc48da86254
Amrit Kumar <kumaramrit38@gmail.com>
2022-02-12 09:47:44 +0530

    first commit

---
+++ src/main/java/com/exampleAll/learn/LearnApplication.java
@@ -0,0 +1,13 @@
+package com.exampleAll.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnApplication.class, args);
+	}
+
+}
