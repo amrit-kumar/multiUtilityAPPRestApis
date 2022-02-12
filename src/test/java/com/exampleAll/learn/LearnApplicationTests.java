a4edb060b02fc05979d32c08c0112e4f248a52e7
Amrit Kumar <kumaramrit38@gmail.com>
2022-02-12 14:44:36 +0530

    added tests

--- src/test/java/com/exampleAll/learn/LearnApplicationTests.java
+++ src/test/java/com/exampleAll/learn/LearnApplicationTests.java
@@ -1,13 +1,20 @@
 package com.exampleAll.learn;
 
+import com.exampleAll.learn.controller.TodoController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LearnApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Autowired
+	private TodoController controller;
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
 
 }
